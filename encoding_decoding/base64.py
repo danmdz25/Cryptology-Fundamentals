beefd56305faafdc013e57c5f048d4385c344b05
Mendes <daniel.mendes01.ti@gmail.com>
2025-08-29 09:58:35 -0300

    Update and rename base64_cryptanalisys.py to base64.py

--- encoding_decoding/base64.py
+++ encoding_decoding/base64.py
@@ -1,9 +1,10 @@
 import base64
 
-with open("base64.txt", "r") as f:
+with open("encoding_decoding/base64.txt", "r") as f:
     texto_base64 = f.read()
 
 texto_decodificado = base64.b64decode(texto_base64).decode('utf-8')
 print(texto_decodificado)
+
 
 
