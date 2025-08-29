525fed8b750ccf0805252a72c8f42db7b431c7ea
Mendes <daniel.mendes01.ti@gmail.com>
2025-08-29 09:59:30 -0300

    Update and rename html_entities_criptanalisys.py to html_entities.py

--- encoding_decoding/html_entities.py
+++ encoding_decoding/html_entities.py
@@ -1,7 +1,8 @@
 import html 
 
-with open('html_entities.txt', 'r') as file:
+with open('encoding_decoding/html_entities.txt', 'r') as file:
     encoded_data = file.read()
     decoded = html.unescape(encoded_data)
 
 print(decoded)
+
