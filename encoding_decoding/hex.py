a0fefe55c2e05f2dd385722a8408cc3d3598aaeb
Mendes <daniel.mendes01.ti@gmail.com>
2025-08-29 09:59:13 -0300

    Update and rename hex_cryptanalisys.py to hex.py

--- encoding_decoding/hex.py
+++ encoding_decoding/hex.py
@@ -1,5 +1,6 @@
-with open('hex_encoding.txt','r') as f:
+with open('encoding_decoding/hex_encoding.txt','r') as f:
     hex_string = f.read()
 
 bytes_object = bytes.fromhex(hex_string).decode('utf-8')
+
 print(bytes_object)
