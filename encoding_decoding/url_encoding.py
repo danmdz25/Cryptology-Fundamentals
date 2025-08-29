f2252b2ecc94980938dffc900ab4e70e2baee0da
Mendes <daniel.mendes01.ti@gmail.com>
2025-08-29 10:00:04 -0300

    Update and rename url_encoding.cryptanalisys.py to url_encoding.py

--- encoding_decoding/url_encoding.py
+++ encoding_decoding/url_encoding.py
@@ -1,7 +1,8 @@
 import urllib.parse
 
-with open("url_encoding.txt", "r") as f:
+with open("encoding_decoding/url_encoding.txt", "r") as f:
     texto_url = f.read()
 
 texto_decodificado = urllib.parse.unquote(texto_url)
+
 print(texto_decodificado)
