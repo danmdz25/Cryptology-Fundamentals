6e8e5f89e556c07cbcd080c7e29968b8d60b6c52
Mendes <daniel.mendes01.ti@gmail.com>
2025-08-29 09:58:00 -0300

    Update and rename base32_cryptanalisys.py to base32.py

--- encoding_decoding/base32.py
+++ encoding_decoding/base32.py
@@ -1,7 +1,8 @@
 import base64
 
-with open("base32.txt","r") as f:
+with open("encoding_decoding/base32.txt","r") as f:
     texto_base32 = f.read()
 
 decoded_text = base64.b32decode(texto_base32).decode('utf-8')
+
 print(decoded_text)
